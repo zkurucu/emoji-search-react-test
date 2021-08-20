37e969bfe232cafb1655e74ca5c2ab1cbc34dc0b
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-08-20 13:58:07 +0300

    Rendering header comp. without crash

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Header from "./Header";
+
 
 // App component without crash
-test("Renders without crashing", () => {
+test("Renders app component without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+});
+
+test("Renders header component without crashing", () => {
+    const div2 = document.createElement("div");
+    ReactDOM.render(<Header />, div2);
 });
