40d605d1273812e4fcaac76e9961b4beb13f05d3
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-08-20 13:55:58 +0300

    React test about rendering app comp. without crash

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
+// App component without crash
+test("Renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
